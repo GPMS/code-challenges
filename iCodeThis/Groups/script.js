fd367c4a702371cc7771ed243f3122cef97c5c1c
Gabriel Portela Macedo Souza <gportelams@gmail.com>
2023-10-10 19:46:32 -0300

    iCodeThis:Groups

---
+++ iCodeThis/Groups/script.js
@@ -0,0 +1,6 @@
+// const displayButtons = document.querySelectorAll("[data-set-display]");
+// const display = document.querySelector("[data-display]");
+
+// displayButtons.forEach(button => button.addEventListener("click", (e) => {
+//     display.dataset.display = e.target.closest("button").dataset.setDisplay
+// }))
