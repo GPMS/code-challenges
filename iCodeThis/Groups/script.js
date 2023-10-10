4a67a1967ed4d5c81b5d9e0fa855bf862bd323af
Gabriel Portela Macedo Souza <gportelams@gmail.com>
2023-10-10 20:00:28 -0300

    iCodeThis:Groups add Tailwind config

--- iCodeThis/Groups/script.js
+++ iCodeThis/Groups/script.js
@@ -1,3 +1,21 @@
+// tailwind settings
+tailwind.config = {
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Rajdhani", "sans-serif"],
+      },
+      colors: {
+        "black-800": "#1A2036",
+        "black-400": "#242B42",
+        "black-100": "#2E3650",
+        "text-900": "#535B77",
+        "text-400": "#A8B0CF",
+      },
+    },
+  },
+};
+
 // const displayButtons = document.querySelectorAll("[data-set-display]");
 // const display = document.querySelector("[data-display]");
 
